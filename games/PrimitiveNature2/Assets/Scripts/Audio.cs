26c1c09e0bccc1624915aa90775c5a4b21c5df7d
James Ballaban <jballaban@gmail.com>
2017-12-27 12:38:00 -0500

    added music

---
+++ games/PrimitiveNature2/Assets/Scripts/Audio.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Audio : audio
+{
+
+	// Use this for initialization
+	public override void Start()
+	{
+		base.Start();
+		playMainMenu();
+	}
+
+	// Update is called once per frame
+	public override void Update()
+	{
+		base.Update();
+	}
+
+	public void playMainMenu()
+	{
+		base.forceIntro();
+	}
+}
