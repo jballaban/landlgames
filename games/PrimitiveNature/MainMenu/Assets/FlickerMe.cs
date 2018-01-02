c3971b9d64bfc9d9f3b27246011184f88bb1ca6e
jballaban@klick.com <jballaban@klick.com>
2018-01-02 18:19:06 -0500

    Scene

---
+++ games/PrimitiveNature/MainMenu/Assets/FlickerMe.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FlickerMe : MonoBehaviour
+{
+
+	private Light firelight;
+
+	// Use this for initialization
+	void Start()
+	{
+		this.firelight = this.GetComponent<Light>();
+	}
+
+	// Update is called once per frame
+	void Update()
+	{
+		this.firelight.intensity = 1 + 0.1f * Mathf.Sin(Time.time * 100);
+	}
+}
